220115b65cc2b68a8f75ac139479c4950bda2950
Varun Nayyar <nayyarv@gmail.com>
2018-07-25 13:51:27 +1000

    some work on the roman numerals q

--- src/RomanNumerals.hs
+++ src/RomanNumerals.hs
@@ -4,6 +4,20 @@ module RomanNumerals where
 -- symb :: [(Int, Char)]
 symbology = [(1000, 'M'), (500, 'D'), (100, 'C'), (50, 'L'), (10, 'X'), (5, 'V'), (1, 'I')]
 
-
 solution :: Integer -> String
 solution n = "hi"
+
+
+solbuilder :: Integer -> Int -> String -> String
+solbuilder currNum symindex outString
+    | symindex == length symbology || currNum <= 0 = outString
+    | numletts == 4 && currRomLet /= 'M' = solbuilder remaining (symindex+1) (outString ++ four)
+    | numletts > 0 && numletts < 4  = solbuilder remaining (symindex+1) (outString ++ take numletts (repeat currRomLet))
+    | otherwise = solbuilder currNum (symindex+1) outString
+    where currRomNum = fst (symbology!!symindex)
+          currRomLet = snd (symbology!!symindex)
+          numletts = fromInteger (currNum `div` currRomNum)
+          remaining = currNum `mod` currRomNum
+          four = [currRomLet, snd (symbology!! (symindex-1))]
+
+
